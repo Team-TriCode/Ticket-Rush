f4e064c47d6a3251bf851eaaf624a18469273ce7
DESKTOP-L2U8RFQ\janho <janhopkins1994@outlook.com>
2018-02-06 21:47:50 +0000

    Basic Mechanics

---
+++ 2D Test/Assets/Scripts/Player_Controller.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Controller : MonoBehaviour {
+
+    private float m_speed = 5.0f;
+    private float m_axisX;
+    private float m_axisY;
+    private float m_jumpPower = 300.0f;
+    private Rigidbody2D m_rb2d;
+    // Use this for initialization
+    void Start ()
+    {
+        m_rb2d = gameObject.GetComponent<Rigidbody2D>();
+	}
+
+    // Update is called once per frame
+
+    private void Update()
+    {
+        PlayerMove();
+        Jump();
+    }
+    
+    private void PlayerMove()
+    {
+        m_axisX = Input.GetAxis("Horizontal");
+
+
+        transform.Translate(new Vector2(m_axisX, m_axisY) * m_speed * Time.deltaTime);
+    }
+    private void Jump()
+    {
+        if(Input.GetButtonDown("Jump"))
+        {
+            m_rb2d.AddForce(Vector2.up * m_jumpPower);
+        }
+
+    }
+}
---
+++ 2D Test/Assets/Scripts/Switch_Controller.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switch_Controller : MonoBehaviour {
+
+    private string m_trigTag;
+    private bool m_inside = false;
+    private string m_colTag;
+
+    private void Start()
+    {
+        m_trigTag = this.tag;
+    }
+    private void Update()
+    {
+        if(m_colTag == "Player" && m_inside == true)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                Switch();
+            } 
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        m_colTag = col.tag;
+        m_inside = true;
+    }
+    private void OnTriggerExit2D(Collider2D col)
+    {
+        m_inside = false;
+    }
+    private void Switch()
+    {
+        switch (m_trigTag)
+        {
+            case "Elevator":
+
+                Debug.Log("in code");
+                GameObject liftPlat = GameObject.FindGameObjectWithTag("Lift_Platform");
+                Lift_Controller liftController = liftPlat.GetComponent<Lift_Controller>();
+                liftController.TurnOn();
+                break;
+
+            case "Elevator1":
+                System.Console.WriteLine("bob");
+                break;
+            case "Elevator2":
+                System.Console.WriteLine("bob");
+                break;
+            case "Elevator3":
+                System.Console.WriteLine("bob");
+                break;
+            default:
+                System.Console.WriteLine("bob");
+                break;
+        }
+    }
+}
