11d3ce9a9c9e4f47b14ddce5f7dc9c4e784acd07
Matt <matthew.d.bell@students.plymouth.ac.uk>
2018-03-19 22:28:41 +0000

    Main Menu Rework
    Updated Main Menu front and back end to stick through the process of updating art assets without being majorly affected

---
+++ 2D Test/Assets/Scripts/MenuScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.SceneManagement;
+
+public class MenuScript : MonoBehaviour
+{
+	public void PlayButton()
+    {
+        EditorSceneManager.LoadScene(1);
+    }
+
+    public void QuitButton()
+    {
+        Application.Quit();
+    }
+}
