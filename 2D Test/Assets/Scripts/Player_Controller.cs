9bb16532d6dc7a8c56b3846cf801d72c4d5cc175
Matt <matthew.d.bell@students.plymouth.ac.uk>
2018-04-12 15:01:43 +0100

    PlayerController methodisation and Test Scene organisation

--- 2D Test/Assets/Scripts/Player_Controller.cs
+++ 2D Test/Assets/Scripts/Player_Controller.cs
@@ -6,8 +6,8 @@ public class Player_Controller : MonoBehaviour
 {
     private float m_health = 100.0f;
     private int m_speed = 5;
-    private int m_jumpHeight = 300;
-    private int m_swingForce = 25;
+    private int m_jumpHeight = 200;
+    private int m_swingForce = 10;
 
     private bool m_isSwinging = false;
     private bool m_isGrounded = true;
@@ -26,90 +26,44 @@ public class Player_Controller : MonoBehaviour
         m_monkey = this.GetComponent<SpriteRenderer>();
         m_anim = this.GetComponent<Animator>();
         m_player = this.GetComponent<Rigidbody2D>();
-    }
-    
+    }    
 
     void Update()
     {
         m_ground = GameObject.FindGameObjectWithTag("Groundcheck1").transform.position;
         m_ground2 = GameObject.FindGameObjectWithTag("Groundcheck2").transform.position;
+
         CheckGrounded();
+        Swinging();
+        Jump();
 
-        if (m_isSwinging == false)
+        this.GetComponent<BoxCollider2D>().enabled = true;
+
+        if (Input.GetButton("A")) // Go left
         {
-            this.GetComponent<BoxCollider2D>().enabled = true;
-            if (Input.GetButton("A"))
+            if (m_look == false)
             {
-                if (m_look == false)
-                {
-                    m_look = true;
-                    m_monkey.flipX = true;
-                }
-
-                m_player.velocity = new Vector3(-m_speed, m_player.velocity.y, 0);
-
-                if (m_isGrounded)
-                {
-                    m_anim.SetInteger("State", 1);
-                }
-
-                if (Input.GetButtonDown("Jump"))
-                {
-                    Jump();
-                }
+                m_look = true;
+                m_monkey.flipX = true;
             }
-            else if (Input.GetButton("D"))
-            {
-                if (m_look == true)
-                {
-                    m_look = false;
-                    m_monkey.flipX = false;
-                }
-                m_player.velocity = new Vector3(m_speed, m_player.velocity.y, 0);
-                if (m_isGrounded)
-                {
-                    m_anim.SetInteger("State", 1);
-                }
-
-                if (Input.GetButtonDown("Jump"))
-                {
-                    Jump();
-                }
-            }
-            else if (Input.GetButtonDown("Jump"))
-            {
-                Jump();
-            }
-            else
-            {
-                m_player.velocity = new Vector3(0, m_player.velocity.y, 0);
-            }
+            m_anim.SetInteger("State", 1);
+            m_player.velocity = new Vector3(-m_speed, m_player.velocity.y, 0);
         }
+        else if (Input.GetButton("D")) // Go right
+        {
+            if (m_look == true)
+            {
+                m_look = false;
+                m_monkey.flipX = false;
+            }
+            m_anim.SetInteger("State", 1);
+            m_player.velocity = new Vector3(m_speed, m_player.velocity.y, 0);
+        }        
         else
         {
-            GetComponent<BoxCollider2D>().enabled = false;
-            m_anim.SetInteger("State", 4);
-
-            if (Input.GetButton("A"))
-            {
-                m_player.AddForce(transform.right * -m_swingForce);
-            }
-
-            if (Input.GetButton("D"))
-            {
-                m_player.AddForce(transform.right * m_swingForce);
-            }
-
-            if (Input.GetButtonDown("Jump"))
-            {
-                m_isSwinging = false;
-                m_anim.SetInteger("State", 5);
-                Destroy(GetComponent<HingeJoint2D>());
-                m_player.AddForce(transform.up * m_jumpHeight);
-                StartCoroutine(Wait());
-            }
+            m_player.velocity = new Vector3(0, m_player.velocity.y, 0);
+            m_anim.SetInteger("State", 0);
         }
-
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -119,17 +73,12 @@ public class Player_Controller : MonoBehaviour
             m_isSwinging = true;
             m_canSwing = false;
 
-            //attach to rope
+            // Attach to rope
             HingeJoint2D hinge = gameObject.AddComponent<HingeJoint2D>() as HingeJoint2D;
             hinge.connectedBody = collision.gameObject.GetComponent<Rigidbody2D>();
-
         }
     }
-    IEnumerator Wait()
-    {
-        yield return new WaitForSeconds(0.1f);
-        m_canSwing = true;
-    }
+        
     private void CheckGrounded()
     {
         Debug.Log(m_ground);
@@ -147,15 +96,17 @@ public class Player_Controller : MonoBehaviour
             m_anim.SetInteger("State", 2);
         }
     }
+
     private void Jump()
     {
-        if (m_isGrounded)
+        if (Input.GetButtonDown("Jump") && m_isGrounded)
         {
             m_player.AddForce(transform.up * m_jumpHeight);
             m_anim.SetInteger("State", 2);
             m_isGrounded = false;
         }
     }
+
     public void TakeDamage(float damage)
     {
         Debug.Log("Hit");
@@ -165,4 +116,36 @@ public class Player_Controller : MonoBehaviour
             Debug.Log("GameOver");
         }
     }
+
+    public void Swinging()
+    {
+        GetComponent<BoxCollider2D>().enabled = false;
+        m_anim.SetInteger("State", 4);
+
+        if (Input.GetButton("A"))
+        {
+            m_player.AddForce(transform.right * -m_swingForce);
+        }
+
+        if (Input.GetButton("D"))
+        {
+            m_player.AddForce(transform.right * m_swingForce);
+        }
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            m_isSwinging = false;
+            m_anim.SetInteger("State", 5);
+            Destroy(GetComponent<HingeJoint2D>());
+            m_player.AddForce(transform.up * m_jumpHeight);
+            StartCoroutine(Wait());
+        }
+    }
+
+    IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(0.1f);
+        m_canSwing = true;
+    }
+
 }
