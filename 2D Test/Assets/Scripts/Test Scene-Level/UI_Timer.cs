b53f0c9fc58b0dfb89b7ae2fdd3d94296ee3a34b
GeorgePlunkettSmith <32201836+GeorgePlunkettSmith@users.noreply.github.com>
2018-05-01 13:09:00 +0100

    Timer
    Added a new Timer that stops on Level Complete

---
+++ 2D Test/Assets/Scripts/Test Scene-Level/UI_Timer.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_Timer : MonoBehaviour {
+
+    public static float timer = 0;
+    private float mins;
+    private float secs;
+    private string minsString;
+    private string secsString;
+    public Text timeString;
+    public Player_Controller pc;
+
+    // Update is called once per frame
+    void Update () {
+
+        if (pc.m_endGame == false)
+        {
+            timer += Time.deltaTime;
+
+            mins = Mathf.Floor(timer / 60);
+
+            if (mins < 10)
+            {
+                minsString = "0" + mins.ToString();
+            }
+
+            else
+            {
+                minsString = mins.ToString();
+            }
+
+            secs = Mathf.RoundToInt(timer % 60);
+
+            if (secs < 10)
+            {
+                secsString = "0" + secs.ToString();
+            }
+
+            else
+            {
+                secsString = secs.ToString();
+            }
+
+            timeString.text = minsString + ":" + secsString;
+        }
+        
+    }
+}
