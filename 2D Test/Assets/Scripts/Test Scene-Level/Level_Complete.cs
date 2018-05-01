b176b3b1d379337c5729d407da0d958d684fc8b1
GeorgePlunkettSmith <32201836+GeorgePlunkettSmith@users.noreply.github.com>
2018-05-01 12:21:22 +0100

    Confetti Ticket Animation
    New Ticket fall animtion when the player wins

--- 2D Test/Assets/Scripts/Test Scene-Level/Level_Complete.cs
+++ 2D Test/Assets/Scripts/Test Scene-Level/Level_Complete.cs
@@ -10,11 +10,13 @@ public class Level_Complete : MonoBehaviour
     public Transform winText;
     public Text scoreVal;
     public Player_Controller pc;
+    public ParticleSystem confetti;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         ShowComplete();
-        Invoke("PlayerWin", 2.5f);
+        confetti.Play();
+        Invoke("PlayerWin", 4.0f);
         pc.m_endGame = true;
     }
 
