8f99a3684b7502bc80cf7837e61c6eb5ad423d28
Vendettan <32261725+Vendettan@users.noreply.github.com>
2018-05-16 13:29:37 +0100

    Time value now affects score - working on leaderbaord not properly sorting scores

--- 2D Test/Assets/Scripts/Test Scene-Level/Level_Complete.cs
+++ 2D Test/Assets/Scripts/Test Scene-Level/Level_Complete.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -40,7 +41,21 @@ public class Level_Complete : MonoBehaviour
 
     private void PlayerWin()
     {
-        PlayerPrefs.SetString("playerScore", scoreVal.text);
+        // Splitting time val around the colon to get minutes and seconds
+        string[] minsSeconds = timeVal.text.Split(':');
+
+        // Converting minutes to seconds to get total seconds
+        int minutes = Convert.ToInt32(minsSeconds[0]) * 60;
+        int seconds = Convert.ToInt32(minsSeconds[1]) + minutes;
+
+        // Editing scoreVal to get just the number value
+        string stringScore = scoreVal.text.Replace("Score: ", "");
+
+        // Converting the edited scoreVal to an int and adding additional score
+        int editedScore = Convert.ToInt32(stringScore) + seconds;
+        string finalScore = Convert.ToString(editedScore);
+
+        PlayerPrefs.SetString("playerScore", finalScore);
         PlayerPrefs.SetString("playerTime", timeVal.text);
         SceneManager.LoadScene("LevelComplete");
     }
